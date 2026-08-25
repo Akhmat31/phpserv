caa370c9dabc24ce2cc0d51e44efcaddfc8e1733
Akhmat31 <hanyaorangbiasa8892@gmail.com>
2026-08-25 11:57:22 +0700

    Message: add detail of why you can't use or register two routes file

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
  * phpserv front controller (Laravel-like)
  *
  * All requests are routed here via .htaccess. It boots the autoloader,
- * builds the app, loads route definitions, and runs the dispatcher.
+ * builds the app, loads route definitions, and runs the dispatcher
  */
 
 use Source\BaseRouter;
@@ -13,6 +13,11 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new BaseRouter();
 $router = $app->router();
 
+/**WARNING
+ * You cannot add or register other routes file
+ * but you can separate using diiferent filename
+ * and use by just require('<path|to|route>.php')
+ */
 (require __DIR__ . '/../routes/web.php')($router);
 
 $app->set404(__DIR__ . '/../resources/err/404.php');
