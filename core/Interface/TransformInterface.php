1c26c4d785698abccebbdc6c695872dc44da7653
Akhmat31 <hanyaorangbiasa8892@gmail.com>
2026-08-26 16:59:19 +0700

    Add: Add API Transform interface

--- core/Interface/TransformInterface.php
+++ core/Interface/TransformInterface.php
@@ -1,6 +1,35 @@
 <?php
+
 namespace Source\Interface;
 
-interface TransformInterface {
-
+interface TransformInterface
+{
+    /**
+     * Transform data to the target format
+     *
+     * @param mixed $data The data to transform
+     * @param string $format The target format (e.g. 'json', 'xml', 'csv', 'array')
+     * @return string|array The transformed data
+     */
+    public function transform(mixed $data, string $format): string|array;
+    /**
+     * Reverse-transform a formatted string back to an array
+     *
+     * @param string $data The formatted string
+     * @param string $format The source format
+     * @return array The decoded array
+     */
+    public function reverseTransform(string $data, string $format): array;
+    /**
+     * Check if a format is supported
+     *
+     * @param string $format The format to check
+     */
+    public function supports(string $format): bool;
+    /**
+     * Get a list of all supported formats
+     *
+     * @return array<int, string>
+     */
+    public function getSupportedFormats(): array;
 }
